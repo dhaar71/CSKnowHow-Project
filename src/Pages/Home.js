1e58f946016c8865d4afcfd956b42a94110594b0
Codder12346768 <dharmip060@gmail.com>
2023-07-13 01:19:24 +0530

    Initial Commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div class="bg-black">
+            <p class="text-white text-center text-5xl">CSKnowHow</p>
+        </div>
+     );
+}
+ 
+export default Home;
